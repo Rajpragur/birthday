36856c6ec03d704dd2310cb94c1836b4cd524b30
Raj Pratap Singh Gurjar <rajpragur@gmail.com>
2025-11-08 05:47:02 +0530

    changes

--- effect.js
+++ effect.js
@@ -3,18 +3,24 @@ $(window).load(function(){
 	$('.container').fadeIn('fast');
 });
 $('document').ready(function(){
-		var vw;
-		$(window).resize(function(){
-			 vw = $(window).width()/2;
-			$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
-			$('#b11').animate({top:240, left: vw-350},500);
-			$('#b22').animate({top:240, left: vw-250},500);
-			$('#b33').animate({top:240, left: vw-150},500);
-			$('#b44').animate({top:240, left: vw-50},500);
-			$('#b55').animate({top:240, left: vw+50},500);
-			$('#b66').animate({top:240, left: vw+150},500);
-			$('#b77').animate({top:240, left: vw+250},500);
-		});
+	var vw;
+	$(window).resize(function(){
+		 vw = $(window).width()/2;
+		 var screenWidth = $(window).width();
+		$('#b1,#b2,#b3,#b4,#b5,#b6').stop();
+		
+		// Responsive balloon spacing on resize
+		var spacing = screenWidth > 768 ? [275, 165, 55] : 
+		              screenWidth > 576 ? [140, 85, 28] : 
+		              screenWidth > 400 ? [110, 66, 22] : [90, 54, 18];
+		              
+		$('#b11').animate({top:240, left: vw-spacing[0]},500);
+		$('#b22').animate({top:240, left: vw-spacing[1]},500);
+		$('#b33').animate({top:240, left: vw-spacing[2]},500);
+		$('#b44').animate({top:240, left: vw+spacing[2]},500);
+		$('#b55').animate({top:240, left: vw+spacing[1]},500);
+		$('#b66').animate({top:240, left: vw+spacing[0]},500);
+	});
 
 	$('#turn_on').click(function(){
 		$('#bulb_yellow').addClass('bulb-glow-yellow');
@@ -24,6 +30,7 @@ $('document').ready(function(){
 		$('#bulb_pink').addClass('bulb-glow-pink');
 		$('#bulb_orange').addClass('bulb-glow-orange');
 		$('body').addClass('peach');
+		$('body').addClass('lights-on');
 		$(this).fadeOut('slow').delay(5000).promise().done(function(){
 			$('#play').fadeIn('slow');
 		});
@@ -37,7 +44,7 @@ $('document').ready(function(){
 		$('#bulb_green').addClass('bulb-glow-green-after');
 		$('#bulb_pink').addClass('bulb-glow-pink-after');
 		$('#bulb_orange').addClass('bulb-glow-orange-after');
-		$('body').css('backgroud-color','#FFF');
+		$('body').css('background-color','#F4C2C2');
 		$('body').addClass('peach-after');
 		$(this).fadeOut('slow').delay(6000).promise().done(function(){
 			$('#bannar_coming').fadeIn('slow');
@@ -94,30 +101,17 @@ $('document').ready(function(){
 			loopSix();
 		});
 	}
-	function loopSeven() {
-		var randleft = 1000*Math.random();
-		var randtop = 500*Math.random();
-		$('#b7').animate({left:randleft,bottom:randtop},10000,function(){
-			loopSeven();
-		});
-	}
-
 	$('#balloons_flying').click(function(){
 		$('.balloon-border').animate({top:-500},8000);
-		$('#b1,#b4,#b5,#b7').addClass('balloons-rotate-behaviour-one');
+		// 6 balloons: RAASHU
+		$('#b1,#b4,#b5').addClass('balloons-rotate-behaviour-one');
 		$('#b2,#b3,#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b3').addClass('balloons-rotate-behaviour-two');
-		// $('#b4').addClass('balloons-rotate-behaviour-one');
-		// $('#b5').addClass('balloons-rotate-behaviour-one');
-		// $('#b6').addClass('balloons-rotate-behaviour-two');
-		// $('#b7').addClass('balloons-rotate-behaviour-one');
 		loopOne();
 		loopTwo();
 		loopThree();
 		loopFour();
 		loopFive();
 		loopSix();
-		loopSeven();
 		
 		$(this).fadeOut('slow').delay(5000).promise().done(function(){
 			$('#cake_fadein').fadeIn('slow');
@@ -141,22 +135,27 @@ $('document').ready(function(){
 		
 	$('#wish_message').click(function(){
 		 vw = $(window).width()/2;
+		 var screenWidth = $(window).width();
 
-		$('#b1,#b2,#b3,#b4,#b5,#b6,#b7').stop();
+		$('#b1,#b2,#b3,#b4,#b5,#b6').stop();
 		$('#b1').attr('id','b11');
 		$('#b2').attr('id','b22')
 		$('#b3').attr('id','b33')
 		$('#b4').attr('id','b44')
 		$('#b5').attr('id','b55')
 		$('#b6').attr('id','b66')
-		$('#b7').attr('id','b77')
-		$('#b11').animate({top:240, left: vw-350},500);
-		$('#b22').animate({top:240, left: vw-250},500);
-		$('#b33').animate({top:240, left: vw-150},500);
-		$('#b44').animate({top:240, left: vw-50},500);
-		$('#b55').animate({top:240, left: vw+50},500);
-		$('#b66').animate({top:240, left: vw+150},500);
-		$('#b77').animate({top:240, left: vw+250},500);
+		
+		// Responsive balloon spacing
+		var spacing = screenWidth > 768 ? [275, 165, 55] : 
+		              screenWidth > 576 ? [140, 85, 28] : 
+		              screenWidth > 400 ? [110, 66, 22] : [90, 54, 18];
+		              
+		$('#b11').animate({top:240, left: vw-spacing[0]},500);
+		$('#b22').animate({top:240, left: vw-spacing[1]},500);
+		$('#b33').animate({top:240, left: vw-spacing[2]},500);
+		$('#b44').animate({top:240, left: vw+spacing[2]},500);
+		$('#b55').animate({top:240, left: vw+spacing[1]},500);
+		$('#b66').animate({top:240, left: vw+spacing[0]},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
 		$(this).fadeOut('slow').delay(3000).promise().done(function(){
@@ -173,9 +172,9 @@ $('document').ready(function(){
 		var i;
 
 		function msgLoop (i) {
-			$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
+			$("p:nth-child("+i+")").fadeOut('slow').delay(1000).promise().done(function(){
 			i=i+1;
-			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
+			$("p:nth-child("+i+")").fadeIn('slow').delay(1500);
 			if(i==50){
 				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
